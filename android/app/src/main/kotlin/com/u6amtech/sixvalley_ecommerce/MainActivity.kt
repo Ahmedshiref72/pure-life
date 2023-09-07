75ad1c021842ff2c96299e7cdcf3defa1bce2630
Ahmed Shiref <103084673+Ahmedshiref72@users.noreply.github.com>
2023-09-07 15:00:50 +0200

    Initial commit

---
+++ android/app/src/main/kotlin/com/u6amtech/sixvalley_ecommerce/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.elseady.onclick
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/utill/custom_themes.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_sixvalley_ecommerce/utill/dimensions.dart';
+
+const titilliumRegular = TextStyle(
+  fontFamily: 'Tajawal',
+  fontSize: Dimensions.FONT_SIZE_SMALL,
+);
+const titleRegular = TextStyle(
+  fontFamily: 'Tajawal',
+  fontWeight: FontWeight.w500,
+  fontSize: Dimensions.FONT_SIZE_DEFAULT,
+
+);
+const titleHeader = TextStyle(
+  fontFamily: 'Tajawal',
+  fontWeight: FontWeight.w600,
+  fontSize: Dimensions.FONT_SIZE_LARGE,
+
+);
+const titilliumSemiBold = TextStyle(
+  fontFamily: 'Tajawal',
+  fontSize: Dimensions.FONT_SIZE_SMALL,
+  fontWeight: FontWeight.w600,
+);
+
+const titilliumBold = TextStyle(
+  fontFamily: 'Tajawal',
+  fontSize: Dimensions.FONT_SIZE_DEFAULT,
+  fontWeight: FontWeight.w700,
+);
+const titilliumItalic = TextStyle(
+  fontFamily: 'Tajawal',
+  fontSize: Dimensions.FONT_SIZE_DEFAULT,
+  fontStyle: FontStyle.italic,
+);
+
+const robotoRegular = TextStyle(
+  fontFamily: 'Tajawal',
+  fontSize: Dimensions.FONT_SIZE_DEFAULT,
+);
+
+const robotoBold = TextStyle(
+  fontFamily: 'Tajawal',
+  fontSize: Dimensions.FONT_SIZE_DEFAULT,
+  fontWeight: FontWeight.w700,
+);
